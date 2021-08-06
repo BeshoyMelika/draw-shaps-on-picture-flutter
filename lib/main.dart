a9175ca8869097050d1d32f445eaa17c1b59bc15
Beshoy Melika <beshoygamal310@gmail.com>
2021-08-06 19:15:28 +0200

    remove unkonw file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-import 'draw_shap.dart';
+import 'package:circles/draw_shape.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
       ),
       home: MyPainter(),
-      // home: Screen1(),
     );
   }
 }
@@ -26,7 +25,6 @@ class MyPainter extends StatefulWidget {
 }
 
 class _MyPainterState extends State<MyPainter> {
-  double scale = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,23 +35,8 @@ class _MyPainterState extends State<MyPainter> {
         children: [
           SizedBox(height: 100),
           InteractiveViewer(
-            onInteractionUpdate: (details) {
-              setState(() {
-                scale = details.scale;
-              });
-            },
             maxScale: 10,
-            child: Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.red,
-              child: Column(
-                children: [
-                  photosss(),
-                  FlutterLogo(),
-                ],
-              ),
-            ),
+            child: PhotoWithShapes(),
           ),
         ],
       ),
@@ -61,7 +44,35 @@ class _MyPainterState extends State<MyPainter> {
   }
 }
 
-List<Shape> sh = [
+class PhotoWithShapes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: SizedBox(
+        height: 400,
+        child: InteractiveViewer(
+          onInteractionStart: (details) {
+            print("Start");
+          },
+          child: Stack(
+            children: [
+              Image.network(
+                  "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/153129468/original/4eb3c930659ce032791104e01abfec9679002e14/2d-maps-for-civil-and-floor-map-house-flats-full-floors.jpg"),
+              DrawShapesWidget(
+                shapes: dummyShapes,
+                onTap: (id) {
+                  print(id);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+List<Shape> dummyShapes = [
   Shape(1, [Offset(140, 150), Offset(150, 180)], ShapeType.Circle,
       Color.fromRGBO(99, 120, 22, .5)),
   Shape(2, [Offset(100, 20), Offset(180, 70)], ShapeType.Rectangle,
@@ -77,36 +88,3 @@ List<Shape> sh = [
       ShapeType.Polygon,
       Color.fromRGBO(299, 12, 122, .5)),
 ];
-
-class photosss extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      child: SizedBox(
-        height: 400,
-        child: InteractiveViewer(
-          onInteractionStart: (details) {
-            print("Start");
-          },
-          child: Stack(
-            children: [
-              Image.network(
-                  "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/153129468/original/4eb3c930659ce032791104e01abfec9679002e14/2d-maps-for-civil-and-floor-map-house-flats-full-floors.jpg"),
-              DrawShapesWidget(
-                shapes: sh,
-                onTap: (id) {
-                  print(id);
-                },
-              ),
-              // Container(
-              //   height: double.infinity,
-              //   width: double.infinity,
-              //   color: Colors.transparent,
-              // )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
